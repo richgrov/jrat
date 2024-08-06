4ee29810d6fb522ad64073cdd61c509ceff31a37
timmy.jinks <tysonjjenkins.github@gmail.com>
2024-08-06 12:08:17 -0600

    Add opencv files

--- resizinator/main.cpp
+++ resizinator/main.cpp
@@ -1,5 +1,10 @@
+#include "opencv2/opencv.hpp"
 #include "common.h"
 
 int main() {
     jrat::test();
+    cv::Mat image;
+    image = cv::imread("corpobs.png");
+    cv::imshow("Original Image", image);
+    cv::waitKey(2000);
 }
