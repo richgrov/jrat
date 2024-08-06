ed8cb19743e8081a688509b3af98a0a5acf9ad2e
AnoyingWaffle66 <joboshua15@gmail.com>
2024-08-06 11:51:18 -0600

    raylib imported (#4)
    * raylib imported and test code
    
    * deleted everything in raylib folder
    
    * raylib correctly imported
    
    * changed subdirectory for raylib
    
    * maybe this time
    
    ---------
    
    Co-authored-by: Josh Arbon <jarbon@student.neumont.edu>

---
+++ resizinator/test.cpp
@@ -0,0 +1,34 @@
+#include "test.h"
+
+void WindowTest::window_test() {
+    // Initialization
+    const int screenWidth = 1280;
+    const int screenHeight = 768;
+    const char *windowTitle = "Hello world!";
+    const char *message = "Hello world! It's great to be here.";
+    const int fontSize = 40;
+    const float msgSpacing = 1.0f;
+
+    InitWindow(screenWidth, screenHeight, windowTitle);
+
+    // NOTE: The following only works after calling InitWindow() (i.e,. RayLib is initialized)
+    const Font font = GetFontDefault();
+    const Vector2 msgSize = MeasureTextEx(font, message, fontSize, msgSpacing);
+    const Vector2 msgPos =
+        Vector2{(screenWidth - msgSize.x) / 2, (screenHeight - msgSize.y) / 2};
+
+    SetTargetFPS(60);
+
+    // Main loop
+    while (!WindowShouldClose()) {
+
+        // Update the display
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+        DrawTextEx(font, message, msgPos, fontSize, msgSpacing, RED);
+        EndDrawing();
+    }
+
+    // Cleanup
+    CloseWindow();
+}
---
+++ resizinator/test.h
@@ -0,0 +1,7 @@
+#pragma once
+#include "raylib.h"
+
+struct WindowTest {
+public:
+    void window_test();
+};
