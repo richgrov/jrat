1e991d82e8225561e651d80622cdac5b1013ff62
timmy.jinks <114427348+timmyjinks@users.noreply.github.com>
2024-08-08 23:08:37 -0600

    Add resizinator methods
    * Add opencv files
    
    * Fix merge conflicts in resizinator
    
    * add resizinator resize methods
    
    * Fix merge conflicts
    
    * Update resizinator.cpp
    
    * Update resizinator.h
    
    * Update resizinator.cpp
    
    * Remove overite options in command line
    
    * Richard is killing me help

--- resizinator/main.cpp
+++ resizinator/main.cpp
@@ -1,17 +1,47 @@
-#include "opencv2/opencv.hpp"
+#include "resizinator.h"
 #include "test.h"
 #include "window_example.h"
+#include <iostream>
+#include <stdlib.h>
 
 using namespace jrat;
 
-int main() {
-    WindowExample example;
-    example.run();
+int main(int argc, char **argv) {
+    // WindowExample example;
+    // example.run();
 
-    cv::Mat image;
-    image = cv::imread("corpobs.png");
-    cv::imshow("Original Image", image);
-    cv::waitKey(2000);
-    WindowTest *window = new WindowTest();
-    window->window_test();
+    if (argv[1] == "--help") {
+        jrat::error_panic();
+        return 0;
+    }
+
+    if (argc < 5 || argc > 6) {
+        error_panic();
+        return 0;
+    }
+
+    std::string image_filepath = argv[1];
+    std::string keep_aspect_ratio = argv[2];
+    int width = std::atoi(argv[3]);
+    int height = std::atoi(argv[4]);
+    std::string new_image_filepath;
+    if (argc == 6) {
+        new_image_filepath = argv[5];
+    }
+    cv::Mat resized_image;
+
+    if (keep_aspect_ratio == "-A") {
+        resized_image = resize_aspect_ratio(image_filepath, width);
+    } else if (keep_aspect_ratio == "-a") {
+        resized_image = resize(image_filepath, width, height);
+    } else {
+        error_panic();
+        return 0;
+    }
+
+    image_filepath = (argc == 5) ? image_filepath : new_image_filepath;
+    imwrite(image_filepath, resized_image);
+
+    /*WindowTest *window = new WindowTest();
+    window->window_test();*/
 }
