6736a49b719499ee6b196622fd45fc6ef9499b01
Josh Arbon <jarbon@student.neumont.edu>
2024-08-09 22:30:49 -0600

    fixed weird indentation

--- common/include/common/textbox.h
+++ common/include/common/textbox.h
@@ -6,15 +6,15 @@
 namespace jrat {
 
 	struct TextBox {
-		public:
+        public:
             TextBox(float x_pos, float y_pos, float width, float height) : area_{Rectangle{x_pos, y_pos, width, height}} {}
 
-			Rectangle get_area() const { return area_; }
+            Rectangle get_area() const { return area_; }
             int get_max_length() const { return max_length_; }
             char content_[9] = {0};
 
         private:
-			Rectangle area_;
+            Rectangle area_;
             int max_length_{8};
 	};
 }
