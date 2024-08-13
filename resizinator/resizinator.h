d815af1702f35832b6c278a9ac55405ab286c0fe
unknown <38992201+richgrov@users.noreply.github.com>
2024-08-12 23:34:12 -0600

    Add spaces between resizinator functions

--- resizinator/resizinator.h
+++ resizinator/resizinator.h
@@ -5,8 +5,13 @@
 #include <opencv2/core/mat.hpp>
 
 namespace jrat {
+
 cv::Mat resize(std::string filepath, int width, int height);
+
 cv::Mat resize_aspect_ratio(std::string filepath, int width);
+
 void imwrite(std::string filepath, cv::Mat image);
+
 void error_panic();
+
 } // namespace jrat
