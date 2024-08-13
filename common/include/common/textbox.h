fc61c12836fea6612b2d9dc9bafe68a89b956875
unknown <38992201+richgrov@users.noreply.github.com>
2024-08-12 23:36:20 -0600

    Fix TextBox indentation, rename get_area to area

--- common/include/common/textbox.h
+++ common/include/common/textbox.h
@@ -4,16 +4,21 @@
 
 namespace jrat {
 
-	struct TextBox {
-        public:
-            TextBox(float x_pos, float y_pos, float width, float height) : area_{Rectangle{x_pos, y_pos, width, height}} {}
+struct TextBox {
+public:
+    TextBox(float x_pos, float y_pos, float width, float height)
+        : area_{Rectangle{x_pos, y_pos, width, height}} {}
 
-            Rectangle get_area() const { return area_; }
-            int get_max_length() const { return max_length_; }
-            char content_[9] = {0};
+    Rectangle area() const {
+        return area_;
+    }
+    int get_max_length() const {
+        return max_length_;
+    }
+    char content_[9] = {0};
 
-        private:
-            Rectangle area_;
-            int max_length_{8};
-	};
-}
+private:
+    Rectangle area_;
+    int max_length_{8};
+};
+} // namespace jrat
