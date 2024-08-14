323136208ab736fcf2d26cdd039acc799edcb898
timmy.jinks <tysonjjenkins.github@gmail.com>
2024-08-14 10:21:23 -0600

    Remove the redundent rotate methods

--- rotatinator/rotatinator.cpp
+++ rotatinator/rotatinator.cpp
@@ -1,26 +1,7 @@
 #include "rotatinator.h"
 #include <iostream>
 
-void jrat::rotate_90_clockwise(cv::Mat image, cv::Mat &rotated_image) {
-    cv::rotate(image, rotated_image, cv::ROTATE_90_CLOCKWISE);
-}
-
-void jrat::rotate_90_counter_clockwise(cv::Mat image, cv::Mat &rotated_image) {
-    cv::rotate(image, rotated_image, cv::ROTATE_90_COUNTERCLOCKWISE);
-}
-
-void jrat::rotate_180(cv::Mat image, cv::Mat &rotated_image) {
-    cv::rotate(image, rotated_image, cv::ROTATE_180);
-}
-
 void jrat::rotate(cv::Mat image, cv::Mat &rotated_image, double angle) {
-    if (angle == 90) {
-        jrat::rotate_90_clockwise(image, rotated_image);
-        return;
-    } else if (angle == -90) {
-        jrat::rotate_90_counter_clockwise(image, rotated_image);
-        return;
-    }
     angle = angle * -1;
     cv::Point2f center((image.cols - 1) / 2.0f, (image.rows - 1) / 2.0f);
     cv::Mat rotation_matrix = cv::getRotationMatrix2D(center, angle, 1.0);
--- rotatinator/rotatinator.h
+++ rotatinator/rotatinator.h
@@ -4,9 +4,6 @@
 #include <opencv2/imgproc.hpp>
 
 namespace jrat {
-void rotate_90_clockwise(cv::Mat image, cv::Mat &rotated_image);
-void rotate_90_counter_clockwise(cv::Mat image, cv::Mat &rotated_image);
-void rotate_180(cv::Mat image, cv::Mat &rotated_image);
 void rotate(cv::Mat image, cv::Mat &rotated_image, double angle);
 } // namespace jrat
 
