b10af93d237e7ab358d73af7a6acbe33a5a5818f
Richard Grover <38992201+richgrov@users.noreply.github.com>
2024-08-16 10:31:39 -0600

    Add all conversion menus, rotate actions (#32)
    * Install remaining executables
    
    * Remove unused include
    
    * Add conversion for all types, move to method
    
    * Use full_command instead of manual concatenation
    
    * Remove redundant jrat prefix
    
    * Add rotate 90 clockwise/counter-clockwise options

--- copy_installer_binaries.sh
+++ copy_installer_binaries.sh
@@ -10,5 +10,10 @@ fi
 
 mkdir -p installer/res
 
+cp $1/flipinator/flipinator$suffix installer/res/flipinator$suffix
 cp $1/convertinator/convertinator$suffix installer/res/convertinator$suffix
+cp $1/cropinator/cropinator$suffix installer/res/cropinator$suffix
+cp $1/resizinator/resizinator$suffix installer/res/resizinator$suffix
+cp $1/rotatinator/rotatinator$suffix installer/res/rotatinator$suffix
+
 cp opencv_world490$lib_suffix installer/res/opencv_world490$lib_suffix
--- installer/main.cpp
+++ installer/main.cpp
@@ -1,6 +1,5 @@
 #include <filesystem>
 #include <fstream>
-#include <iterator>
 #include <stdexcept>
 #include <vector>
 
@@ -58,6 +57,42 @@ void add_dll_path() {
     env_vars.set_string("Path", path);
 }
 
+struct MenuOption {
+    std::string display;
+    std::string id;
+    std::string command;
+};
+
+void add_menu(
+    const std::string &display, const std::string &id, const std::vector<MenuOption> &options,
+    const std::string &file_pattern
+) {
+    RegistryKey command_store(
+        RegistryKey::LOCAL_MACHINE,
+        "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Explorer\\CommandStore\\shell"
+    );
+
+    std::string option_list;
+    for (const MenuOption &option : options) {
+        std::string full_id = id + "." + option.id;
+
+        RegistryKey command(command_store, full_id);
+        command.set_string("MUIVerb", option.display);
+        RegistryKey(command, "command").set_string("", option.command);
+
+        option_list += full_id + ";";
+    }
+
+    RegistryKey menu(RegistryKey::CLASSES_ROOT, "*\\shell\\" + id);
+    menu.set_string("MUIVerb", display);
+    menu.set_string("SubCommands", option_list);
+    menu.set_string("AppliesTo", file_pattern);
+}
+
+std::string full_command(const std::string &app_name, const std::string args = "") {
+    return (APP_DIR / app_name).string() + " " + args;
+}
+
 } // namespace
 
 int main(int argc, char **argv) {
@@ -65,24 +100,31 @@ int main(int argc, char **argv) {
 
     add_dll_path();
 
+    install_file("FLIPINATOR_EXE", "flipinator.exe");
     install_file("CONVERTINATOR_EXE", "convertinator.exe");
+    install_file("CROPINATOR_EXE", "cropinator.exe");
+    install_file("RESIZINATOR_EXE", "resizinator.exe");
+    install_file("ROTATINATOR_EXE", "rotatinator.exe");
     install_file("OPENCV_DLL", "opencv_world490.dll");
 
     std::string supported_file_list = build_supported_file_list(supported_types);
 
-    RegistryKey menu(RegistryKey::CLASSES_ROOT, "*\\shell\\jrat");
-    menu.set_string("MUIVerb", "JRAT");
-    menu.set_string("SubCommands", "jrat.resize");
-    menu.set_string("AppliesTo", supported_file_list);
+    std::vector<MenuOption> operations = {
+        {"Rotate 90 clockwise", "rotate90cw", full_command("rotatinator.exe", "%1 90 %1")},
+        {"Rotate 90 counter-clockwise", "rotate90ccw", full_command("rotatinator.exe", "%1 -90 %1")
+        },
+    };
+    add_menu("JRAT", "jrat", operations, supported_file_list);
 
-    RegistryKey command_store(
-        RegistryKey::LOCAL_MACHINE,
-        "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Explorer\\CommandStore\\shell"
-    );
+    std::vector<MenuOption> conversions;
+    for (const std::string &type : supported_types) {
+        conversions.push_back(MenuOption{
+            .display = "Convert to ." + type,
+            .id = "convert." + type,
+            .command = full_command("convertinator.exe", " %1 " + type),
+        });
+    }
+    add_menu("JRAT: Convert", "jratconvert", conversions, supported_file_list);
 
-    RegistryKey resize_command(command_store, "jrat.resize");
-    resize_command.set_string("MUIVerb", "Convert to .png");
-    RegistryKey(resize_command, "command")
-        .set_string("", (APP_DIR / "convertinator.exe %1 png").string());
     return 0;
 }
