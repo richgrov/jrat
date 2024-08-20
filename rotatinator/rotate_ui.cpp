d21e5d1f3fa5372438d6f286d824f143fb446614
Andrew Bell <abell@student.neumont.edu>
2024-08-20 10:40:39 -0600

    Implement UI

---
+++ rotatinator/rotate_ui.cpp
@@ -0,0 +1,28 @@
+#include "rotate_ui.h"
+#include "rotatinator.h"
+#include "common/window.h"
+
+using namespace jrat;
+
+RotateUi::RotateUi(int width, int height, const std::string &title, const char *file_name, cv::Mat &image)
+    : Window(width, height, title) {
+    load_font();
+    load_image(file_name);
+    set_dimensions_and_position();
+    ui_boxes();
+
+    open_image_ = image;
+    save_file_ = file_name;
+}
+
+void RotateUi::update() {}
+
+void RotateUi::draw() {}
+
+void RotateUi::save_image() {
+    write_image(open_image_, angle_, save_file_);
+}
+
+void RotateUi::ui_boxes() {
+    create_text_box_left(1);
+}
