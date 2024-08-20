fa32666e8f5db440a63c0ebc4b82c93df08db6f6
timmy.jinks <tysonjjenkins.github@gmail.com>
2024-08-20 15:50:46 -0600

    Fix rotation bug that doesn't resize correctly

--- rotatinator/rotatinator.cpp
+++ rotatinator/rotatinator.cpp
@@ -1,6 +1,6 @@
 #include "rotatinator.h"
-#include <opencv2/opencv.hpp>
 #include <iostream>
+#include <opencv2/opencv.hpp>
 
 void jrat::print_help() {
     std::cout << "[filepath] [degrees] [rotated image file path]\n";
@@ -8,11 +8,17 @@ void jrat::print_help() {
 
 void jrat::write_image(cv::Mat image, double angle, std::string savepath) {
     cv::Mat rotated_image;
-
-    angle = angle * -1;
-
+    // image to rotate in the center
     cv::Point2f center((image.cols - 1) / 2.0f, (image.rows - 1) / 2.0f);
-    cv::Mat rotation_matrix = cv::getRotationMatrix2D(center, angle, 1.0);
-    cv::warpAffine(image, rotated_image, rotation_matrix, image.size());
+    cv::Mat rotation_matrix = cv::getRotationMatrix2D(center, -angle, 1.0);
+
+    // get the bounding box
+    cv::Rect2f bounding_box = cv::RotatedRect(cv::Point2f(), image.size(), angle).boundingRect2f();
+
+    rotation_matrix.at<double>(0, 2) += bounding_box.width / 2.0 - image.cols / 2.0;
+    rotation_matrix.at<double>(1, 2) += bounding_box.height / 2.0 - image.rows / 2.0;
+
+    // ROTATE!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    cv::warpAffine(image, rotated_image, rotation_matrix, bounding_box.size());
     cv::imwrite(savepath, rotated_image);
 }
