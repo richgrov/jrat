0448058a27c741ec00811a68e8aafa74ebb5025f
Andrew Bell <abell@student.neumont.edu>
2024-08-20 11:34:45 -0600

    Remove unused method declaration

--- rotatinator/rotatinator.h
+++ rotatinator/rotatinator.h
@@ -6,7 +6,6 @@ class cv::Mat;
 
 namespace jrat {
 
-void rotate(cv::Mat image, cv::Mat &rotated_image, double angle);
 void print_help();
 void write_image(cv::Mat image, double angle, std::string savepath);
 
