b6fed9cff4deb2eb1f9f5d55e322f5c9cde8f3fe
Andrew Bell <abell@student.neumont.edu>
2024-08-20 11:10:59 -0600

    Merge rotate() into write_image()

--- rotatinator/rotatinator.cpp
+++ rotatinator/rotatinator.cpp
@@ -2,20 +2,17 @@
 #include <opencv2/opencv.hpp>
 #include <iostream>
 
-void jrat::rotate(cv::Mat image, cv::Mat &rotated_image, double angle) {
-    angle = angle * -1;
-    cv::Point2f center((image.cols - 1) / 2.0f, (image.rows - 1) / 2.0f);
-    cv::Mat rotation_matrix = cv::getRotationMatrix2D(center, angle, 1.0);
-    cv::warpAffine(image, rotated_image, rotation_matrix, image.size());
-}
-
 void jrat::print_help() {
     std::cout << "[filepath] [degrees] [rotated image file path]\n";
 }
 
 void jrat::write_image(cv::Mat image, double angle, std::string savepath) {
-    cv::Mat resized_image;
+    cv::Mat rotated_image;
 
-    jrat::rotate(image, resized_image, angle);
-    cv::imwrite(savepath, resized_image);
+    angle = angle * -1;
+
+    cv::Point2f center((image.cols - 1) / 2.0f, (image.rows - 1) / 2.0f);
+    cv::Mat rotation_matrix = cv::getRotationMatrix2D(center, angle, 1.0);
+    cv::warpAffine(image, rotated_image, rotation_matrix, image.size());
+    cv::imwrite(savepath, rotated_image);
 }
