02a2a8f09722fc4d5f4578a11c65b63169ad673f
unknown <38992201+richgrov@users.noreply.github.com>
2024-08-21 02:16:12 -0600

    Move main logic to entrypoint.cpp

--- common/include/common/entrypoint.h
+++ common/include/common/entrypoint.h
@@ -1,50 +1,7 @@
-#ifdef JRAT_ENTRYPOINT
-#error "entrypoint.h included more than once"
-#else
-#define JRAT_ENTRYPOINT
-#endif
+#pragma once
 
-#if defined(WIN32) || defined(_WIN32) || defined(__WIN32) && !defined(__CYGWIN__)
+namespace jrat {
 
-#include <Windows.h>
-#include <iostream>
+extern int run(int argc, char **argv);
 
-int run(int argc, char **argv);
-
-#ifdef JRAT_WINMAIN
-
-// NOLINTBEGIN(misc-definitions-in-headers)
-int WINAPI WinMain(HINSTANCE instance, HINSTANCE prev, LPSTR command, int show_command) {
-    // NOLINTEND(misc-definitions-in-headers)
-    char *args[16];
-    LPSTR cmd_line = GetCommandLine();
-
-    int argc = 0;
-    bool new_arg = true;
-
-    for (char *c = cmd_line; *c != '\0'; ++c) {
-        if (new_arg) {
-            args[argc++] = c;
-            if (argc >= sizeof(args)) {
-                std::cerr << "warning: some args were truncated\n";
-            }
-            new_arg = false;
-        } else if (*c == ' ') {
-            *c = '\0';
-            new_arg = true;
-        }
-    }
-
-    return run(argc, args);
-}
-
-#else // if NDEBUG
-
-// NOLINTBEGIN(misc-definitions-in-headers)
-int main(int argc, char **argv) {
-    // NOLINTEND(misc-definitions-in-headers)
-    return run(argc, argv);
-}
-
-#endif // else
-#endif // if windows
+} // namespace jrat
