9974a392364b9d1db2a98d65257397610f71a35d
unknown <38992201+richgrov@users.noreply.github.com>
2024-08-22 22:56:15 -0600

    Add save logic

--- cropinator/ui.cpp
+++ cropinator/ui.cpp
@@ -1,14 +1,34 @@
 #include "ui.h"
 
+#include <opencv2/core/mat.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <string>
+
 using namespace jrat;
 
-CropUi::CropUi(const char *filepath) : Window(1280, 720, "Resize Image", filepath) {
+CropUi::CropUi(const char *filepath)
+    : Window(1280, 720, "Resize Image", filepath), filepath_(filepath) {
     load_font();
     load_image(filepath);
     set_dimensions_and_position();
     ui_boxes();
 }
 
+void CropUi::save_image() {
+    int top = get_textbox(0);
+    int left = get_textbox(1);
+    int bottom = get_textbox(2);
+    int right = get_textbox(3);
+
+    cv::Mat original = cv::imread(filepath_, cv::IMREAD_UNCHANGED);
+    cv::Mat cropped = original(cv::Range(top, bottom - top), cv::Range(left, right - left));
+    cv::imwrite(filepath_, cropped);
+}
+
 void CropUi::ui_boxes() {
     create_text_box_left(4);
+}
+
+int CropUi::get_textbox(int index) {
+    return std::stod(std::string(text_boxes_[index].content_));
 }
--- cropinator/ui.h
+++ cropinator/ui.h
@@ -10,9 +10,14 @@ public:
 
     virtual void ui_boxes() override;
     virtual void update() override {}
-    virtual void save_image() override {}
+    virtual void save_image() override;
 
     virtual void draw() override {}
+
+private:
+    int get_textbox(int index);
+
+    std::string filepath_;
 };
 
 } // namespace jrat
