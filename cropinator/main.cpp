1c6ada4ee29bc6e13b93833dbdd06a84a1b84510
timmy.jinks <tysonjjenkins.github@gmail.com>
2024-08-27 10:20:37 -0600

    Remove test code

--- cropinator/main.cpp
+++ cropinator/main.cpp
@@ -7,7 +7,6 @@
 #include "ui.h"
 
 int jrat::run(int argc, char *argv[]) {
-    argv[1] = (char*)"C:/Users/tjenkins/Pictures/cursed.webp";
     if (argc < 1) {
         std::cerr << "usage: cropinator <file name> [left] [right] [top] [bottom]\n";
         return 1;
