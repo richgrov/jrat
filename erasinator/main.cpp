59bf4f170439a74c00520757b3b019bf6396cfaa
unknown <38992201+richgrov@users.noreply.github.com>
2024-08-26 23:54:04 -0600

    Initial commit

---
+++ erasinator/main.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+
+#include <opencv2/core/mat.hpp>
+#include <opencv2/dnn.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/imgproc.hpp>
+
+namespace dnn = cv::dnn;
+
+namespace {
+
+cv::Mat predict_segment_mask(dnn::Net &net, cv::Mat image) {
+    cv::Mat input;
+    cv::cvtColor(image, input, cv::COLOR_BGR2RGB);
+
+    cv::Mat blob = dnn::blobFromImage(
+        input, 1. / 255., cv::Size(1024, 1024), cv::Scalar(0, 0, 0), false, false
+    );
+    net.setInput(blob);
+
+    cv::Mat raw_out = net.forward();
+
+    cv::Mat out_img = raw_out.reshape(1, 1024) * 255;
+    cv::normalize(out_img, out_img, 0, 255, cv::NORM_MINMAX);
+    cv::resize(out_img, out_img, cv::Size(image.cols, image.rows));
+    out_img.convertTo(out_img, CV_8U);
+    return out_img;
+}
+
+} // namespace
+
+int main(int argc, char **argv) {
+    dnn::Net net = dnn::readNet("model.onnx");
+
+    cv::Mat image = cv::imread("Image.jpg", cv::IMREAD_COLOR);
+    if (image.data == nullptr) {
+        std::cerr << "not found\n";
+        return 1;
+    }
+
+    std::vector<cv::Mat> channels(3);
+    cv::split(image, channels);
+    channels.push_back(predict_segment_mask(net, image));
+
+    cv::Mat result;
+    cv::merge(channels, result);
+
+    cv::imwrite("out.png", result);
+}
